e646dc4232fecf9584748265af7ce1312d55fc16
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-06 17:07:04 -0300

    Fix an light Header Component styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,27 +8,30 @@ import logoWhite from '../assets/logo-white.png'
 
 export function Header(){
     return (
-        <div className="flex gap-12 items-center width-full border-b-1.5 border-b-primary sm:p-6">
-            <Link href="/">
-                <Image alt="" src={logoWhite} />
-            </Link>
+        <header className="width-full border-b-primary border-b-1.5 ">
+            <nav className="max-w-desktop mx-auto flex gap-12 items-center sm:p-6">
 
-            <button className={
-                    "font-medium flex items-center gap-1 text-primary sm:text-xl " +
-                    "transition-all ease-linear duration-150 hover:text-shadow-light "
-                } 
-            >
-                Conversas
-                <CaretDown className="sm:h-6 sm:w-6" />
-            </button>
+                <Link href="/">
+                    <Image alt="" src={logoWhite} />
+                </Link>
 
-            <Link href="/login" className={
-                "bg-primary text-white font-bold sm:py-2 sm:px-4 sm:text-2xl rounded ml-auto " +
-                "transition-opacity  hover:opacity-75"
-                }
-            >
-                Login
-            </Link>
-        </div>
+                <button className={
+                        "font-medium flex items-center gap-1 text-primary sm:text-xl " +
+                        "transition-all ease-linear duration-150 hover:text-shadow-light "
+                    } 
+                >
+                    Conversas
+                    <CaretDown className="sm:h-6 sm:w-6" />
+                </button>
+
+                <Link href="/login" className={
+                    "bg-primary text-white font-bold sm:py-2 sm:px-4 sm:text-2xl rounded ml-auto " +
+                    "transition-opacity  hover:opacity-75"
+                    }
+                >
+                    Login
+                </Link>
+            </nav>
+        </header>
     )
 }
