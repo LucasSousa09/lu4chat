47f55b15ebdd248bc15bc029dad15405808d04be
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-09 09:56:44 -0300

    Update TitleText to mobile view of my-chat page

--- src/components/TitleText.tsx
+++ src/components/TitleText.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { useContext } from 'react'
+import { ArrowLeft } from '@phosphor-icons/react'
+
+import { RoomSidebarContext } from '@/context/RoomSidebarContext'
+
 type TitleTextProps = {
     text: string
+    returnButton?: boolean
 }
 
-export function TitleText({text}: TitleTextProps){
+export function TitleText({text, returnButton = false}: TitleTextProps){
+    const { setSidebarIsOpen } = useContext(RoomSidebarContext)
+
     return (
-        <strong className="text-primary font-medium text-xl sm:text-[28px] whitespace-nowrap mb-6">
+        <strong className="flex items-center gap-2 text-primary font-medium text-xl sm:text-[28px] whitespace-nowrap mb-6">
+            <button
+                onClick={() => setSidebarIsOpen(state => !state)} 
+                className={`md:hidden ${!returnButton && 'hidden'}`}>
+                <ArrowLeft />
+            </button>
             {text}
         </strong> 
     )
