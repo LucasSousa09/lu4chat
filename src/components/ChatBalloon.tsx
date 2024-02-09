b4206498cd3245be1dda7ad0e29da699e08a777a
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-09 09:51:59 -0300

    Create and style Chat Balloon

---
+++ src/components/ChatBalloon.tsx
@@ -0,0 +1,34 @@
+type ChatBalloonProps = {
+    sender: string
+    message: string
+    userMessage?: boolean
+    evenMessage?: 'even' | 'odd' | undefined
+}
+
+export function ChatBalloon({sender, message, userMessage = false, evenMessage = undefined}: ChatBalloonProps){
+    return (
+        <div 
+            className={
+                "flex flex-col rounded-lg p-2 gap-1 min-w-80 max-w-[calc(50%-12px)] " +
+                `${evenMessage === 'even' ? 'bg-secondary ' : (evenMessage === 'odd' ? 'bg-white border border-secondary ' : 'bg-terciary ')}` +
+                `${userMessage ? 'ml-auto' : 'ml-0'}` 
+            }>
+            <strong 
+                className={
+                    "text-sm font-bold " +
+                    `${evenMessage === 'even' ? 'text-white' : 'text-secondary'}` 
+                }
+            >
+                {sender}
+            </strong>
+
+            <span 
+                className={
+                    `${evenMessage === 'even' ? 'text-white' : 'text-secondary'}` 
+                }
+            >
+                {message}
+            </span> 
+        </div>
+    )
+}
