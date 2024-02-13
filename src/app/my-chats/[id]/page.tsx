462e92433950ceb7dfcee771d02d8d9f1abfaafd
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-13 20:29:34 -0300

    Add my chats page

---
+++ src/app/my-chats/[id]/page.tsx
@@ -0,0 +1,67 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
+import { RoomSidebar } from "@/components/RoomSidebar";
+import { RoomInfoBoxMyChat } from "@/components/RoomInfoBoxMyChat";
+import { MessagesContainer } from "@/components/MessagesContainer";
+import { RoomSidebarProvider } from "@/context/RoomSidebarContext";
+import { MessageSenderForm } from "@/components/MessageSenderForm";
+
+import { api } from "@/libs/axios";
+
+export default async function Chats({params}: {params: {id: string}}){
+    const session = await getServerSession()
+
+    //Verify if chat exists
+    const resIfChatExists = await api.post('/verify-if-chat-exists', {chatID: params.id})
+
+    const chatExistsData = JSON.parse(resIfChatExists.data)
+
+    //If chat does not exists redirect to Public Rooms
+    if(chatExistsData === null){
+        redirect('/public-rooms')
+    }
+
+    //Verify if user is logged, if not redirect to login
+    if(session === null){
+        redirect('login')
+    }
+
+    //Verify if the user already participates at the chat, if not adds to the user chat list
+    const res = await api.post('/add-room-to-user', {email: session.user?.email, room: `${params.id}`})
+
+    let userRoomsData
+
+    if(res.data === 'This room is private'){
+        redirect('/private-room-access')
+    }
+
+    if(res.status === 200){
+        userRoomsData = res.data.userRoomsArray
+    }
+    else{
+        console.error(res.data)
+    }
+
+    return (
+        <div className="relative bg-white max-w-desktop mx-auto flex h-[calc(100vh-52px)] sm:h-[calc(100vh-80px)] md:h-[calc(100vh-100px)]">
+            <RoomSidebarProvider>
+
+                <RoomSidebar>
+                    {
+                        userRoomsData.length > 0 &&
+                        userRoomsData.map((room: string) => (
+                            <RoomInfoBoxMyChat key={room} roomId={room} currentRoom={params.id === room ? true : false} />
+                        ))
+                    }
+                </RoomSidebar>
+
+                <main className="flex flex-col-reverse h-full w-full">
+                    <MessageSenderForm roomId={params.id} senderId={res.data.userId} sender={session.user?.name || ""}/>
+
+                    <MessagesContainer roomId={params.id} roomName={chatExistsData.name} />
+                </main>
+            </RoomSidebarProvider>
+        </div>
+    )
+}
--- src/app/my-chats/page.tsx
+++ src/app/my-chats/page.tsx
@@ -1,46 +1,31 @@
-import { PaperPlaneRight } from '@phosphor-icons/react/dist/ssr'
+import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth"
+import { collection, getDocs, query, where } from "firebase/firestore"
 
-import { RoomSidebarProvider } from '../../context/RoomSidebarContext'
+import { db } from "@/libs/firebase";
 
-import { TitleText } from '@/components/TitleText'
-import { ChatBalloon } from '@/components/ChatBalloon'
-import { RoomInfoBox } from '@/components/RoomInfoBox'
-import { RoomSidebar } from '@/components/RoomSidebar'
+export default async function MyChats(){
+    let redirectToRoom = ""
+    const session = await getServerSession()
+    const usersRef =  collection(db, "users");
 
-export default function MyChats(){
-    return (
-        <div className="relative bg-white max-w-desktop mx-auto flex h-[calc(100vh-52px)] sm:h-[calc(100vh-80px)] md:h-[calc(100vh-100px)]">
-            <RoomSidebarProvider>
+    if(session){
+        const q = query(usersRef, where("email", "==", session.user?.email))
 
-                <RoomSidebar>
-                    <RoomInfoBox enterRoom={false} currentRoom roomType='Pública' roomName='Honkai Star Rail' roomDescription='Para todos os fãs de Star Rail' />
-                    <RoomInfoBox enterRoom={false} roomType='Privada' roomName='Grupo da Família' roomDescription='Grupo especial da família' />
-                </RoomSidebar>
+        const querySnapshot = await getDocs(q)
 
-                <main className="flex flex-col-reverse h-full w-full">
-                    <form className="flex items-center gap-4 w-full max-w-[100vw] p-4 border-t border-primary" action="">
-                        <input 
-                            className="h-[44px] sm:h-[52px] w-full border-2 border-primary text-secondary rounded py-3 px-4" 
-                            type="text" 
-                            />
-                        <button 
-                            className="flex items-center gap-2 rounded bg-secondary text-white font-bold text-sm sm:text-xl px-4 py-3"
-                            >
-                            Enviar
-                            <PaperPlaneRight className="" weight='fill' />
-                        </button>
-                    </form>
-                    
-                    <div className="flex-1 p-6">
-                        <TitleText returnButton text='Honkai Star Rail' />
-                        <div className="flex flex-col mt-6 gap-2">
-                            <ChatBalloon evenMessage='odd' sender='Nickolas' message='Penacony finalmente chegou!!!!' />
-                            <ChatBalloon userMessage sender='Lucas' message='Verdade, espero pegar a Black Suan... Já foram 30 draws e nada ainda...' />
-                            <ChatBalloon evenMessage='even' sender='Caio' message='Nunca mais nem loguei, kkkk' />
-                        </div>
-                    </div>
-                </main>
-            </RoomSidebarProvider>
-        </div>
-    )
+        if(querySnapshot.empty){
+            redirect('/public-rooms')    
+        }
+
+        querySnapshot.forEach((doc) => {
+            const user = doc.data()
+
+            redirectToRoom = user.rooms[0]
+        })
+  
+        redirect(`/my-chats/${redirectToRoom}`)
+    }
+
+    redirect('/login')
 }
