910688017ad57aeab94b08ce2ee7e51dfcc959ab
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-14 14:58:27 -0300

    Update RoomInfoBox to fit only the public-rooms pages

--- src/components/RoomInfoBox.tsx
+++ src/components/RoomInfoBox.tsx
@@ -1,49 +1,41 @@
-'use client'
-
-import { RoomSidebarContext } from "@/context/RoomSidebarContext"
-import { useContext } from "react"
+import Link from "next/link"
 
 type RoomInfoBoxProps = {
-    roomName: string
-    roomType: 'public' | 'private'
-    roomDescription: string
+    roomId: string
+
+    roomName?: string
+    roomType?: 'public' | 'private'
+    roomDescription?: string
 
     enterRoom?: boolean
-
-    currentRoom?: boolean
 }
 
-export function RoomInfoBox({ roomName, roomType, roomDescription, currentRoom = false, enterRoom = true }: RoomInfoBoxProps){
-    const { setSidebarIsOpen } = useContext(RoomSidebarContext)
-
-    return (
-        <div 
-            onClick={() => setSidebarIsOpen(state => !state)}
-            className={
-                "w-full rounded mb-3 " +
-                `${currentRoom ? 'border-none bg-secondary' : 'border border-primary'}` 
-            }
-        >
-            <header 
+export function RoomInfoBox({ roomId, roomName, roomDescription, roomType }: RoomInfoBoxProps){
+        return(
+            <div 
                 className={
-                    "flex items-center justify-between p-2 border-b " +
-                    `${currentRoom ? 'border-white text-white' : 'border-primary text-primary'}`
+                    "w-full rounded mb-3 border border-primary " 
                 }
             >
-                <strong >{roomName}</strong>
-                <span className="font-medium text-sm">{roomType === 'private' ? 'Privada' : 'Pública'}</span>
-            </header>
-            <div className="flex items-center justify-between px-2 py-3">
-                <span className="text-terciary text-sm">{roomDescription}</span>
-                <button 
+                <header 
                     className={
-                        "p-[6px] rounded font-medium text-sm " +
-                        `${currentRoom ? 'bg-white text-secondary' : 'bg-primary text-white'}`
+                        "flex items-center justify-between p-2 border-b border-primary text-primary "
                     }
                 >
-                       {enterRoom ? 'Entrar' : 'Sair'}
-                </button>
+                    <strong >{roomName}</strong>
+                    <span className="font-medium text-sm">{roomDescription}</span>
+                </header>
+                <div className="flex items-center justify-between px-2 py-3">
+                    <span className="text-terciary text-sm">{roomType === "private" ? "Privada" : "Pública"}</span>
+                    <Link
+                        href={`/my-chats/${roomId}`} 
+                        className={
+                            "p-[6px] rounded font-medium text-sm bg-primary text-white "
+                        }
+                    >
+                        Entrar
+                    </Link>
+                </div>
             </div>
-        </div>
-    )
+        )
 }
