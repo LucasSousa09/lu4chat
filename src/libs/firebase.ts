a854f0f2db033b2d26b9dcbe5d84dc1945ff5ad6
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-14 16:10:29 -0300

    Add .env.example file and small adjust to firebaseConfig

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -2,14 +2,14 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database'
 import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.FIREBASE_PROJECT_ID || "",
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.FIREBASE_APP_ID || ""
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  databaseURL: "https://lu4chat-default-rtdb.firebaseio.com/",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
