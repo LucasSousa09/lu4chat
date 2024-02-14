4ef774e1aac8bc4c5bed1567ad9769e51e4be947
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-14 14:51:03 -0300

    Update Container Component to show loading status on my-chats pathname

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,6 +3,7 @@
 import { ReactNode, useState } from "react"
 import { Header } from "./Header"
 import { CreateRoomForm } from "./CreateRoomForm"
+import { usePathname } from "next/navigation"
 
 type ContainerProps = {
     children: ReactNode
@@ -11,10 +12,21 @@ type ContainerProps = {
 export function Container({ children }: ContainerProps ){
     const [ modalIsOpen, setModalIsOpen ] = useState(false)
 
+    const pathName = usePathname()
+
     return (
         <div className="w-full h-full bg-secondary">
             <Header setModalIsOpen={setModalIsOpen}/>
             <CreateRoomForm modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}/>
+            {
+                pathName === '/my-chats' && (
+                    <div className="flex items-start justify-center bg-white max-w-desktop h-[calc(100vh-100px)] mx-auto pt-12">
+                        <strong className=" bg-secondary py-6 px-8 rounded text-white text-4xl animate-pulse">
+                            Loading...
+                        </strong>
+                    </div>
+                )
+            }
             {children}
         </div>
     )
