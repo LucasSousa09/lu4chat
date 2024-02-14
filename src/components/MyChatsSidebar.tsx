e1775dd06b49ffe885f2d52a759f3ce2feb80fb0
lucas <lucasrodrigues.sousa09@gmail.com>
2024-02-14 18:57:24 -0300

    Adjust small style bugs fom MyChatSidebar

--- src/components/MyChatsSidebar.tsx
+++ src/components/MyChatsSidebar.tsx
@@ -14,14 +14,14 @@ export function MyChatsSidebar({ children } : MyChatsSidebarProps){
     
     return (
         <div className={
-            "bg-white absolute top-0 left-0 bottom-0 md:static md:translate-x-0 transition-transform ease-linear " + 
+            "w-full max-w-96 bg-white absolute top-0 left-0 bottom-0 md:static md:translate-x-0 transition-transform ease-linear " + 
             "md:w-96 h-full border-r border-primary py-3 px-1 " +
             `${sidebarIsOpen ? 'translate-x-0' : '-translate-x-96'}`
             }
         >
             <button
                 onClick={() => setSidebarIsOpen(false)} 
-                className="flex text-primary ml-auto mb-3 pr-1"
+                className="flex md:hidden text-primary ml-auto mb-3 pr-1"
             >
                 <X weight="bold" className="h-5 w-5"/>
             </button>
